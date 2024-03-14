3f1aa1c1cbabec5fbb8a475f7f114a3ad23c4d5b
Eren Akgun <yeren@sabanciuniv.edu>
2024-03-14 12:42:21 +0100

    Models are created and initializations are done.

---
+++ models.py
@@ -0,0 +1,57 @@
+class Product:
+    def __init__(self, sku, description, sales_price, purchase_price):
+        self.sku = sku
+        self.description = description
+        self.sales_price = sales_price
+        self.purchase_price = purchase_price
+
+    def to_json(self):
+        return {
+            'SKU': self.sku,
+            'Description': self.description,
+            'SellingPrice': self.sales_price,
+            'PurchasePrice': self.purchase_price
+        }
+
+class Client:
+    def __init__(self, name, email, shipping_address, phone):
+        self.name = name
+        self.email = email
+        self.shipping_address = shipping_address
+        self.phone = phone
+
+    def to_json(self):
+        return {
+            'Name': self.name,
+            'Email': self.email,
+            'Address': self.shipping_address,
+            'Phone': self.phone
+        }
+
+class Supplier:
+    def __init__(self, name, email, shipping_address, phone):
+        self.name = name
+        self.email = email
+        self.shipping_address = shipping_address
+        self.phone = phone
+
+    def to_json(self):
+        return {
+            'Name': self.name,
+            'Email': self.email,
+            'Address': self.shipping_address,
+            'Phone': self.phone
+        }
+
+class InventoryLocation:
+    def __init__(self, abbreviation, name, address):
+        self.abbreviation = abbreviation
+        self.name = name
+        self.address = address
+
+    def to_json(self):
+        return {
+            'Abbreviation': self.abbreviation,
+            'Name': self.name,
+            'Address': self.address
+        }
